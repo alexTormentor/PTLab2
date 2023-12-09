8e86ed09d04a977d6ab23eb3b40fddc2814ab4a2
Alex <alexanderbor0134@mail.ru>
2023-12-09 16:22:19 +0300

    models

--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
+    alcohol_type = models.CharField(max_length=50, default='Без регистрации')
+    quantity = models.PositiveIntegerField(default=0)
+
+    def decrease_quantity(self):
+        if self.quantity > 0:
+            self.quantity -= 1
+            self.save()
 
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
