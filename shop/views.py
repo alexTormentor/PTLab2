fda833a8a17cc87cf6e6fe4f5df81c6495cc0b20
Alex <alexanderbor0134@mail.ru>
2023-12-10 20:58:29 +0300

    new views

--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,8 @@ from django.views.generic.edit import CreateView
 
 from .models import Product, Purchase
 from .forms import PurchaseForm
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
@@ -24,11 +26,30 @@ class PurchaseCreate(CreateView):
     model = Purchase
     form_class = PurchaseForm
     template_name = 'shop/purchase_form.html'
-    success_url = 'cart/'
+    success_url = reverse_lazy('purchase_success')
 
     def form_valid(self, form):
-        form.instance.product_id = self.kwargs['product_id']
-        return super().form_valid(form)
+        product_id = self.kwargs['product_id']
+        form.instance.product_id = product_id
+        form.save()
+
+        product = Product.objects.get(pk=product_id)
+        person = self.request.POST.get('person', '')
+        address = self.request.POST.get('address', '')
+
+        return render(self.request, 'shop/purchase_success.html',
+                      {'product': product, 'person': person, 'address': address})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_id = self.kwargs['product_id']
+        product = Product.objects.get(pk=product_id)
+        context['product_id'] = product_id
+        context['product'] = product
+        context['quantity'] = 1
+        context['person'] = self.request.POST.get('person', '')
+        context['address'] = self.request.POST.get('address', '')
+        return context
 
 
 def cart(request):
@@ -55,3 +76,49 @@ def edit_cart(request):
     total_items = sum(item.quantity for item in cart_items)
     context = {'cart_items': cart_items, 'total_items': total_items}
     return render(request, 'shop/edit_cart.html', context)
+
+
+class PurchaseSuccess(TemplateView):
+    template_name = 'shop/purchase_success.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_id = self.request.GET.get('product_id')
+        product = Product.objects.get(pk=product_id)
+        person = self.request.GET.get('person', '')
+        address = self.request.GET.get('address', '')
+
+        context['product'] = product
+        context['person'] = person
+        context['address'] = address
+
+        return context
+
+
+class PurchaseCreateAll(CreateView):
+    model = Purchase
+    form_class = PurchaseForm
+    template_name = 'shop/purchase_form.html'
+    success_url = reverse_lazy('purchase_success_all')
+
+    def form_valid(self, form):
+        cart_items = Product.objects.filter(quantity__gt=0)
+
+        for item in cart_items:
+            Purchase.objects.create(product=item, person=form.cleaned_data['person'],
+                                    address=form.cleaned_data['address'])
+
+        context = {'cart_items': cart_items, 'person': form.cleaned_data['person'], 'address': form.cleaned_data['address']}
+        return render(self.request, 'shop/purchase_success.html', context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_id'] = None
+
+        cart_items = Product.objects.filter(quantity__gt=0)
+        context['quantity'] = sum(item.quantity for item in cart_items)
+
+        context['cart_items'] = cart_items
+        context['person'] = self.request.POST.get('person', '')
+        context['address'] = self.request.POST.get('address', '')
+        return context
