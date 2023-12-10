9dee30cfc154084765e60b38ca3817bb94875ce6
Alex <alexanderbor0134@mail.ru>
2023-12-10 20:58:22 +0300

    new url path

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('buy/', views.PurchaseCreate.as_view(), name='buy'),
+    path('buy/<int:product_id>/', views.PurchaseCreate.as_view(), name='buy'),
+    path('buy_all/', views.PurchaseCreateAll.as_view(), name='buy_all'),
+    path('purchase_success/', views.PurchaseSuccess.as_view(), name='purchase_success'),
     path('cart/', views.cart, name='cart'),
     path('edit_cart/', views.edit_cart, name='edit_cart'),
 ]
